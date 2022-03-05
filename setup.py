e59b67909454506be1853964bde9fae804f31731
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-03-05 10:36:37 +0330

    Update to new version 0.2

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="AutoLMS",
-    version="0.1",
+    version="0.2",
     packages=["autolms"],
     url="https://github.com/itsamirhn/AutoLMS",
-    download_url="https://github.com/itsamirhn/AutoLMS/archive/refs/tags/v0.1.tar.gz",
+    download_url="https://github.com/itsamirhn/AutoLMS/archive/refs/tags/v0.2.tar.gz",
     license="MIT",
     author="AmirMohammad Hosseini Nasab",
     author_email="awmirhn@gmail.com",
